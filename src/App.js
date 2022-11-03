96b339bb10156a0f52ef425f32cec9ea7024193c
annastmal <103998851+annastmal@users.noreply.github.com>
2022-11-03 16:53:12 +0000

    made new repository

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import DictionarySearch from './DictionarySearch';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <main>
+          <DictionarySearch/>
+        </main>
       </header>
     </div>
   );
