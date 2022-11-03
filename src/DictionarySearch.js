761c57c0c22c8197f100467dec2d28b5c54aefb4
annastmal <103998851+annastmal@users.noreply.github.com>
2022-11-03 17:19:58 +0000

    added search field

--- src/DictionarySearch.js
+++ src/DictionarySearch.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function DictionarySearch(){
-    return(
-        "hELLO"
-    )
+export default function DictionarySearch() {
+  let [keyword, setKeyword] = useState("");
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword} definition`);
+  }
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" onChange={handleKeywordChange} />
+      </form>
+    </div>
+  );
 }
