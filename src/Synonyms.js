0082bace0228f106ccdec12208c437b02d7b1386
annastmal <103998851+annastmal@users.noreply.github.com>
2022-11-09 16:35:57 +0000

    added  world "similar"

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -4,9 +4,11 @@ export default function Synonyms(props) {
   if (props.synonyms) {
     return (
       <ul className="Synonyms">
+        <span><strong>Similar: </strong><em></em>
         {props.synonyms.map((synonym, index) => {
           return <li key={index}>{synonym}</li>;
         })}
+        </span>
       </ul>
     );
   } else {
