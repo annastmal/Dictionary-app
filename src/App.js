17a2a009d2d0834cde52835b06d8dc5c1fbf7bf6
annastmal <103998851+annastmal@users.noreply.github.com>
2022-12-05 16:02:19 +0000

    added footer

--- src/App.js
+++ src/App.js
@@ -8,10 +8,20 @@ function App() {
     <div className="App">
       <header className="App-header ">
         <main>
-          <DictionarySearch className="search"/>
-          <ParticlesBG className="particles"/>
+          <DictionarySearch className="search" />
+          <ParticlesBG className="particles" />
         </main>
       </header>
+      <footer className="footer">
+        <p>
+          Designed &{" "}
+          <a href="https://github.com/annastmal/Dictionary-app" class="footer_text" target="blank">
+            {" "}
+            open-sourced coded
+          </a>{" "}
+          by Anna Malanchuk
+        </p>
+      </footer>
     </div>
   );
 }
